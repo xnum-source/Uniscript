3c06eb73419fc324ad42d11d736f39e1b3eb17c5
Xyzdesk <65450641+Xyzdesk@users.noreply.github.com>
2020-07-02 11:51:28 +0100

    Add Source to Repository (#1)
    Added sources to repository.

---
+++ Uniscript/Console/Source/Uniscript CLI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Uniscript_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine(@"Uniscript 3.9.18.8 Command Line Interface
+Created by Xyzdesk
+
+To get more information about this specific type, go to xyzdesk.online/docs/3-9-18-8/about
+");
+
+            
+            
+            // new ln
+            Console.Write(Environment.UserName + Environment.MachineName + " # (DEFAULT), Executing NU.xs1 in " + Environment.OSVersion);
+            Console.WriteLine(@"");
+            Console.Write(">>> ");
+            // string command = Console.ReadLine();
+            string command = Console.ReadLine();
+
+            if (command == "cdir()")
+            {
+                Console.WriteLine("Current Directory: " + Environment.CurrentDirectory);
+            }
+            if(command == "help()")
+            {
+                
+            }
+            
+           
+        }
+    }
+}
